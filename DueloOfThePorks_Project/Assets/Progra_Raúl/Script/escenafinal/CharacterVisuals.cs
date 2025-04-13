f4e829d0b2c5c2c0d1b1dd53404f7b278969340a
RaulCoretB <raulcoret8@gmail.com>
2025-04-13 20:21:55 +0200

    FinalScene_Mejorada

---
+++ DueloOfThePorks_Project/Assets/Progra_Raúl/Script/escenafinal/CharacterVisuals.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class CharacterVisuals : MonoBehaviour
+{
+    public GameObject victoryPrefab;  // Prefab con animación de victoria
+    public GameObject defeatPrefab;   // Prefab con animación de derrota
+    public AudioClip victorySound;    // Sonido de victoria
+}
