ed0b1d4dd7a6761bd14b0f32cc03ea6580126ce0
RaulCoretB <raulcoret8@gmail.com>
2025-04-20 12:11:54 +0200

    ANIMACIONES

--- DueloOfThePorks_Project/Assets/Progra_Raúl/Script/CharacterAudioController.cs
+++ DueloOfThePorks_Project/Assets/Progra_Raúl/Script/CharacterAudioController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+Ôªøusing UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 public class CharacterAudioController : MonoBehaviour
@@ -10,33 +10,23 @@ public class CharacterAudioController : MonoBehaviour
     public AudioClip upAttack;
     public AudioClip downAttack;
     public AudioClip strongAttack;
-    public AudioClip specialAttack;
-    public AudioClip jumpAttack;
+    public AudioClip specialAttack;    // ‚Üê agregado
 
     [Header("Otros Sonidos")]
     public AudioClip jump;
     public AudioClip dash;
-    public AudioClip hurt;
 
     void Awake()
     {
         src = GetComponent<AudioSource>();
     }
 
-    // MÈtodos p˙blicos para reproducir cada sonido
-    public void PlayBaseAttack() => Play(baseAttack);
-    public void PlayUpAttack() => Play(upAttack);
-    public void PlayDownAttack() => Play(downAttack);
-    public void PlayStrongAttack() => Play(strongAttack);
-    public void PlaySpecialAttack() => Play(specialAttack);
-    public void PlayJumpAttack() => Play(jumpAttack);
+    public void PlayBaseAttack() { if (baseAttack != null) src.PlayOneShot(baseAttack); }
+    public void PlayUpAttack() { if (upAttack != null) src.PlayOneShot(upAttack); }
+    public void PlayDownAttack() { if (downAttack != null) src.PlayOneShot(downAttack); }
+    public void PlayStrongAttack() { if (strongAttack != null) src.PlayOneShot(strongAttack); }
+    public void PlaySpecialAttack() { if (specialAttack != null) src.PlayOneShot(specialAttack); }  // ‚Üê agregado
 
-    public void PlayJump() => Play(jump);
-    public void PlayDash() => Play(dash);
-    public void PlayHurt() => Play(hurt);
-
-    private void Play(AudioClip clip)
-    {
-        if (clip != null) src.PlayOneShot(clip);
-    }
+    public void PlayJump() { if (jump != null) src.PlayOneShot(jump); }
+    public void PlayDash() { if (dash != null) src.PlayOneShot(dash); }
 }
